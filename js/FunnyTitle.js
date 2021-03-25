8457187ee8f363732c35ae3d2adeaccab5eb01da
Sekiro <1293739194@qq.com>
2021-03-24 22:47:14 -0800

    Update FunnyTitle.js

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -3,12 +3,12 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/666WXY666/cdn@10.4/img/favicon/funny.ico");
+        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/666WXY666/cdn@10.5/img/favicon/funny.ico");
         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/666WXY666/cdn@10.4/img/favicon/favicon.ico");
+        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/666WXY666/cdn@10.5/img/favicon/favicon.ico");
         document.title = '(ฅ>ω<*ฅ) 噫又好啦 ~' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
